b2304387f7bacbf54b5f2d301951a70c1bb3c743
sinya228 <131292437+sinya228@users.noreply.github.com>
2023-06-13 20:14:43 +0300

    Сборная солянка

---
+++ Assets/Development/Keyboard scripts/Components/KeyboardComponent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct KeyboardComponent
+{
+    public Dictionary<string,string> keyborDictionary;
+}
