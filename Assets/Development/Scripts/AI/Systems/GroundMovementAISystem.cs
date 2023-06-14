67f00e36cfcb3ab6f2acd4e60efc5cd0fd7a23bf
sinya228 <131292437+sinya228@users.noreply.github.com>
2023-06-14 22:35:52 +0300

    доработка бота

--- Assets/Development/Scripts/AI/Systems/GroundMovementAISystem.cs
+++ Assets/Development/Scripts/AI/Systems/GroundMovementAISystem.cs
@@ -1,5 +1,6 @@
 ﻿using Assets.Development.Scripts.AI.Components;
 using Leopotam.Ecs;
+using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -14,36 +15,34 @@ namespace Assets.Development.Scripts.AI
         {
             foreach (var i in AI_Filter)
             {
-                Debug.Log("пришли в AI");
                 ref var gMovAIComponent = ref AI_Filter.Get1(i);
-                if (gMovAIComponent.moveDistance == 0)
-                {
-                    gMovAIComponent.moveDistance = 20f;
-                    Debug.Log("назначили дистанцию");
-                }
+                //if (gMovAIComponent.moveDistance == 0)
+                //{
+                //    gMovAIComponent.moveDistance = 20f;
+                //    Debug.Log("назначили дистанцию");
+                //}
                 if (gMovAIComponent._position != gMovAIComponent._transform.position)
                 {
                     gMovAIComponent._position = gMovAIComponent._transform.position;
-                    Debug.Log("назначили положение");
                 }
             }
         }
         public void Run()
         {
-            Debug.Log("говно");
             foreach (var i in AI_Filter)
             {
-                Debug.Log("пришли в AI");
                 ref var gMovAIComponent = ref AI_Filter.Get1(i);
 
                 Debug.Log(gMovAIComponent._position);
                 Debug.Log(gMovAIComponent._transform.position);
-
-                if (gMovAIComponent._position.x == gMovAIComponent._transform.position.x && gMovAIComponent._position.z == gMovAIComponent._transform.position.z)
+                if (gMovAIComponent._position.x - 2 < gMovAIComponent._transform.position.x &&
+                    gMovAIComponent._position.x + 2 > gMovAIComponent._transform.position.x &&
+                    gMovAIComponent._position.z - 2 < gMovAIComponent._transform.position.z &&
+                    gMovAIComponent._position.z + 2 > gMovAIComponent._transform.position.z 
+                     )
                 {
                     gMovAIComponent._position = RandomNavSphere(gMovAIComponent.moveDistance, gMovAIComponent._transform);
                     gMovAIComponent._navMeshAgent.SetDestination(gMovAIComponent._position);
-                    Debug.Log("Изменили положение");
                 }
 
 
